e0f54f278a5c7096a349d3f3dc6f2fdb3560dffd
Jatin Singh <clonicalclone@gmail.com>
2025-11-07 21:02:17 +0530

    fixed errors

--- src/Components/Newsletter.tsx
+++ src/Components/Newsletter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import '../App.css'
 import { ToastContainer, toast, Slide } from 'react-toastify';
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Navbar from '../Components/Navbar'
 import MidHome from '../Components/MidHome'
 import FeaturedProducts from '../Components/FeaturedProducts'
