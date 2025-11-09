e5d5a33fb7a695a768916ab72cef9dc272794e02
Jatin Singh <clonicalclone@gmail.com>
2025-11-09 15:12:12 +0530

    Reworked SEO

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,6 @@ export default defineConfig(({ mode }) => ({
   build: {
     sourcemap: false
   },
- server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-    headers:
-      mode === 'development'
-        ? {} // ✅ no CSP in dev
-        : {
-            "Content-Security-Policy":
-              "default-src 'none'; base-uri 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; frame-ancestors 'self'; object-src 'none'; form-action 'self'; upgrade-insecure-requests;"
-          }
-  },
   plugins: [
     react({
       babel: {
