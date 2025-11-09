aa46e913d225a0279bf46fe64727e8acb54a1c33
Jatin Singh <clonicalclone@gmail.com>
2025-11-09 15:32:12 +0530

    Reworked SEO

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(() => ({
+export default defineConfig({
+  base: '/', // ✅ Ensures correct asset URLs on Vercel
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-    }),tailwindcss()
-  ]
-}))
+    }),
+    tailwindcss(),
+  ],
+})
