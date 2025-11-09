683ad11c8971379b5005968b194de32251718475
Jatin Singh <clonicalclone@gmail.com>
2025-11-09 15:14:52 +0530

    Reworked SEO

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  build: {
-    sourcemap: false
-  },
+export default defineConfig(() => ({
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),tailwindcss()
-  ], define: {
-    __DEV__: mode === 'development'
-  }
+  ]
 }))
